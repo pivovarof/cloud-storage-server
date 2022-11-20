d8838c2af0023fa31d5d5d32db107479896ba9af
pivovarof <lgagrog@gmail.com>
2022-11-20 11:35:38 +0200

    test commit

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -41,6 +41,7 @@ class FileController {
     }
   }
   async uploadFile(req, res) {
+    console.log(req);
     try {
       const file = req.files.file;
 
@@ -70,12 +71,16 @@ class FileController {
       file.mv(path);
 
       const type = file.name.split('.').pop();
+      let filePath = file.name;
+      if (parent) {
+        filePath = parent.path + '\\' + file.name;
+      }
 
       const dbFile = new File({
         name: file.name,
         type,
         size: file.size,
-        path: parent?.path,
+        path: filePath,
         parent: parent?._id,
         user: user._id,
       });
@@ -93,14 +98,10 @@ class FileController {
   async downloadFile(req, res) {
     try {
       const file = await File.findOne({ _id: req.query.id, user: req.user.id });
+
       const path =
-        config.get('filePath') +
-        '\\' +
-        req.user.id +
-        '\\' +
-        file.path +
-        '\\' +
-        file.name;
+        config.get('filePath') + '\\' + req.user.id + '\\' + file.path;
+
       if (fs.existsSync(path)) {
         return res.download(path, file.name);
       }
@@ -108,6 +109,21 @@ class FileController {
     } catch (error) {
       console.log(error);
       res.status(500).json({ message: 'Download error' });
+    }
+  }
+
+  async deleteFile(req, res) {
+    try {
+      const file = await File.findOne({ _id: req.query.id, user: req.user.id });
+      if (!file) {
+        return res.status(400).json({ message: 'file not found' });
+      }
+      fileService.deleteFile(file);
+      await file.remove();
+      return res.json({ message: 'file has been deleted' });
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({ message: 'Dir is not empty' });
     }
   }
 }
